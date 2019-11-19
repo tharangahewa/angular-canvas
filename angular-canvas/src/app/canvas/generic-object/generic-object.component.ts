4c820cea38ea04cb31d79d311ff9c4e1c6e5bfa6
Tharanga Hewavithana <tharanga.hewavithana@cinglevue.com>
2019-11-19 17:00:28 +0530

    added resizabilty and z-index support

--- angular-canvas/src/app/canvas/generic-object/generic-object.component.ts
+++ angular-canvas/src/app/canvas/generic-object/generic-object.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { IResizeEvent } from "angular2-draggable/lib/models/resize-event";
+import { IPosition } from "angular2-draggable";
 
 @Component({
   selector: 'app-generic-object',
@@ -7,25 +9,46 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class GenericObjectComponent implements OnInit {
 
-  @Input()
-  bounds:HTMLElement
+  styles: any;
 
-  constructor() { }
+  @Input()
+  bounds: HTMLElement;
+  @Input()
+  order: number;
+  position: IPosition;
+
+  constructor() {
+  }
 
   ngOnInit() {
+    const colors = ['green', 'red', 'yellow'];
+    this.styles = {
+      'background-color': colors[this.order - 1]
+    }
+    this.position = {} as IPosition;
+    this.position.x = this.order * 30;
+    this.position.y = this.order * 30;
   }
 
   onStart(event: any) {
-    // console.log("onStart");
-    console.log(event);
+    console.log("onStart moving");
+
   }
 
   onStop(event: any) {
-    // console.log("onStop");
-    console.log(event);
+    console.log("onStop moving");
   }
 
   onMoveEnd(event) {
-    console.log( "x=" + event.x + " y=" + event.y)
+    console.log("x=" + event.x + " y=" + event.y)
   }
+
+  onRzStart(event: IResizeEvent) {
+    console.log("onStart resizing");
+  }
+
+  onRzStop(event: IResizeEvent) {
+    console.log("width=" + event.size.width + " height=" + event.size.height)
+  }
+
 }
